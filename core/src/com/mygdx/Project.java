58c7cb8ddea911fad732bacf263430c15b53aa1b
NewLifeStyle101 <damian.myszko@gmail.com>
2024-03-06 14:46:43 +0000

    Changed game class name to project so can inherit from gdx game class

---
+++ core/src/com/mygdx/Project.java
@@ -0,0 +1,31 @@
+package com.mygdx;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.pages.MainMenu;
+
+public class Project extends Game{
+	public SpriteBatch batch;
+	public BitmapFont font;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		this.setScreen(new MainMenu(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
