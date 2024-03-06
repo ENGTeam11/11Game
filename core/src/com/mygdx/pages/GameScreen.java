018dcadcf02d5eeea22818284cfa46c0d4dc3885
NewLifeStyle101 <damian.myszko@gmail.com>
2024-03-06 14:46:53 +0000

    Added game screen for the game logic

---
+++ core/src/com/mygdx/pages/GameScreen.java
@@ -0,0 +1,55 @@
+package com.mygdx.pages;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Game;
+
+public class GameScreen implements Screen{
+
+    private Game game;
+    public GameScreen(Game game){
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'show'");
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resize'");
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pause'");
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resume'");
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hide'");
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dispose'");
+    }
+
+}
