724ad0d0f4b77395f82f6acc484e6b5622dc3485
NewLifeStyle101 <damian.myszko@gmail.com>
2024-03-06 20:03:05 +0000

    Deleted unused dependencies

--- core/src/com/mygdx/Project.java
+++ core/src/com/mygdx/Project.java
@@ -1,20 +1,20 @@
 package com.mygdx;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.pages.MainMenu;
 
 public class Project extends Game{
 	public SpriteBatch batch;
+	public ShapeRenderer shape;
 	public BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		shape = new ShapeRenderer();
 		font = new BitmapFont();
 		this.setScreen(new MainMenu(this));
 	}
