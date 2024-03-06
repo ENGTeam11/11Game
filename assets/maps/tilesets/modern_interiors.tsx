42474924d7a7825075379ce728d6b2cfd32064b7
zubshaikk <zubairahmed.shaik@outlook.com>
2024-03-06 15:33:37 +0000

    Updated Map

--- assets/maps/tilesets/modern_interiors.tsx
+++ assets/maps/tilesets/modern_interiors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="modern_interiors" tilewidth="16" tileheight="16" tilecount="1424" columns="16">
- <image source="../assets/tileset/modern_interiors/Modern tiles_Free/Interiors_free/16x16/Interiors_free_16x16.png" width="256" height="1424"/>
+ <image source="../assets/Interiors_free_16x16.png" width="256" height="1424"/>
 </tileset>
---
+++ assets/maps/tilesets/person_sitting1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="person_sitting1" tilewidth="16" tileheight="16" margin="16" tilecount="11" columns="11">
+ <image source="../assets/tileset/modern_interiors/Modern tiles_Free/Characters_free/Adam_sit3_16x16.png" width="192" height="32"/>
+</tileset>
