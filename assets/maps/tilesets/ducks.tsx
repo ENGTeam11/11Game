97b2e282944e76c08b510543a545fe1037d4aba6
zubshaikk <zubairahmed.shaik@outlook.com>
2024-03-13 16:09:47 +0000

    Changed ducks

--- assets/maps/tilesets/ducks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="ducks" tilewidth="16" tileheight="16" tilecount="96" columns="12">
- <image source="../assets/ducky_3_spritesheet.png" width="192" height="128"/>
-</tileset>
