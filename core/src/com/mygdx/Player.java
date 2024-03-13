7a0173b5bb669c8330bc9dc6b3cbb6f7407735c5
Yousif <yz6075@york.ac.uk>
2024-03-13 06:37:19 +0000

    Importing Player class
    Player class has basic animations and movement logic.

---
+++ core/src/com/mygdx/Player.java
@@ -0,0 +1,140 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+
+public class Player {
+    //variables
+    private static final float FRAME_TIME = 1 / 5f; //5 fps
+    private final Texture texture;
+    private float elapsed_time;
+    private Animation<TextureRegion> idle;
+    private Animation<TextureRegion> runRight;
+    private Animation<TextureRegion> runLeft;
+    private Animation<TextureRegion> runUp;
+    private Animation<TextureRegion> runDown;
+    private SpriteBatch batch;
+    private Vector2 position;
+    private float speed;
+
+    //movement variables
+    private boolean isIdle;
+    private boolean isMovingLeft;
+    private boolean isMovingRight;
+    private boolean isMovingUp;
+    private boolean isMovingDown;
+
+
+    public Player(Texture texture, float x, float y, float speed){
+        this.texture = texture;
+        this.position = new Vector2(x, y);
+        this.speed = speed;
+    }
+
+
+    public void create(){
+        TextureAtlas allAnimations = new TextureAtlas(Gdx.files.internal("allAnimationsAtlas.atlas"));
+
+        //idle TextureAtlas
+        idle = new Animation<>(FRAME_TIME, allAnimations.findRegions("left_idle"));
+        idle.setPlayMode(Animation.PlayMode.LOOP);
+
+        //run right TextureAtlas
+        runRight = new Animation<>(FRAME_TIME, allAnimations.findRegions("right_walk"));
+        runRight.setPlayMode(Animation.PlayMode.LOOP);
+
+        //run left animation
+        runLeft = new Animation<>(FRAME_TIME, allAnimations.findRegions("left_walk"));
+        runLeft.setPlayMode(Animation.PlayMode.LOOP);
+
+        //run up animation
+        runUp = new Animation<>(FRAME_TIME, allAnimations.findRegions("up_walk"));
+        runUp.setPlayMode(Animation.PlayMode.LOOP);
+
+        //run down animation
+        runDown = new Animation<>(FRAME_TIME, allAnimations.findRegions("down_walk"));
+        runDown.setPlayMode(Animation.PlayMode.LOOP);
+
+
+        batch = new SpriteBatch();
+
+    }
+
+
+    public void render(float delta){
+        elapsed_time += delta;
+        System.out.println("Player position: " + position);
+
+
+        Animation<TextureRegion> animation = idle;
+        if (isMovingRight){
+            animation = runRight;
+        }
+        if (isMovingLeft){
+            animation = runLeft;
+        }
+        if (isMovingDown){
+            animation = runDown;
+        }
+        if (isMovingUp){
+            animation = runUp;
+        }
+
+        TextureRegion currentFrame = animation.getKeyFrame(elapsed_time, true);
+
+
+        batch.begin();
+        batch.draw(currentFrame, position.x, position.y, currentFrame.getRegionWidth() / 0.7f, currentFrame.getRegionHeight() / 0.7f);
+        batch.end();
+    }
+
+
+
+    public void dispose(){
+        batch.dispose();
+        texture.dispose();
+    }
+
+    //moving boolean statements
+    public void moveUp(){
+        position.y += speed;
+        isMovingUp = true;
+    }
+
+    public void moveDown(){
+        position.y -= speed;
+        isMovingDown = true;
+    }
+
+    public void moveLeft(){
+        position.x -= speed;
+        isMovingLeft = true;
+    }
+
+    public void moveRight(){
+        position.x += speed;
+        isMovingRight = true;
+    }
+
+    public void stopMovingUp(){
+        isMovingUp = false;
+    }
+    public void stopMovingDown (){
+        isMovingDown = false;
+    }
+
+    public void stopMovingRight(){
+        isMovingRight = false;
+    }
+
+    public void stopMovingLeft(){
+        isMovingLeft = false;
+    }
+
+}
