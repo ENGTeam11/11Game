3f31871b524d9202bc739c084a52bcbbc59620dd
zubshaikk <zubairahmed.shaik@outlook.com>
2024-03-19 19:27:53 +0000

    Removed unnecessary classes

--- desktop/src/com/mygdx/DesktopLauncher.java
+++ desktop/src/com/mygdx/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.mygdx;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.Project;
 
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
