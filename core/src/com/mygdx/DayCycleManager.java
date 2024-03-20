1017e5708dd8e48c600c8a36102693aa33123f6e
zubshaikk <zubairahmed.shaik@outlook.com>
2024-03-20 15:44:33 +0000

    added DayCycle and Time

---
+++ core/src/com/mygdx/DayCycleManager.java
@@ -0,0 +1,94 @@
+package com.mygdx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
+public class DayCycleManager {
+    private int day;
+    private int hour;
+    private int minute;
+    private final int maxDays = 7;
+    private BitmapFont font;
+    private ShapeRenderer shapeRenderer;
+    private SpriteBatch dayBatch;
+    private float timeAccumulator = 0;
+
+
+    public DayCycleManager() {
+        this.day = 1;
+        this.hour = 8;
+        this.minute = 0;
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        shapeRenderer = new ShapeRenderer();
+        dayBatch = new SpriteBatch();
+    }
+
+    public void update(float deltaTime){
+        timeAccumulator += deltaTime;
+
+        while (timeAccumulator >= 1) {
+            minute++;
+            timeAccumulator -= 1;
+
+            if (minute >= 60) {
+            minute = 0;
+            hour++;
+                if (hour >= 24) {
+                hour = 0;
+                nextDay();
+                }
+            }
+        }
+    }
+
+    public void addTime(int hours) {
+        // Time manipulation logic...
+    }
+
+    public void nextDay() {
+        day++;
+    }
+
+    public String getTime() {
+        return String.format("%02d:%02d", hour, minute);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void render() {
+        float yStart = Gdx.graphics.getHeight() - 70; // Adjust based on your energy bar's position
+        float width = 65; // Width of the box
+        float height = 25; // Height of each box
+
+
+        // Draw background shapes
+
+        shapeRenderer.begin(ShapeType.Filled);
+        shapeRenderer.setColor(Color.DARK_GRAY);
+
+        // Day box
+        shapeRenderer.rect(25, yStart, width, height);
+
+        // Time box, positioned below the Day box
+        shapeRenderer.rect(25, yStart - height - 5, width, height); // 5 pixels gap between boxes
+        shapeRenderer.end();
+
+        // Draw text
+        dayBatch.begin(); // Begin SpriteBatch to draw text
+        font.draw(dayBatch, "Day - " + getDay(), 30, yStart + height - 7); // Adjust text alignment as needed
+        font.draw(dayBatch, getTime(), 30, yStart - 12); // Time text, adjusted for the gap
+        dayBatch.end();
+    }
+
+    public void dispose() {
+        font.dispose();
+        shapeRenderer.dispose();
+    }
+}
--- core/src/com/mygdx/GameScreen.java
+++ core/src/com/mygdx/GameScreen.java
@@ -50,6 +50,7 @@ public class GameScreen extends ScreenAdapter {
     private GameMap gameMap;
 
     private EnergyMeter energyMeter;
+    private DayCycleManager dayCycleManager;
 
     public GameScreen(Game game, Skin skin) {
         this.game = game;
@@ -69,7 +70,7 @@ public class GameScreen extends ScreenAdapter {
         createInsufficient();
         currentTable = null;
 
-
+        dayCycleManager = new DayCycleManager();
         gameMap = new GameMap("maps/map.tmx");
 
         Vector2 spawnPoint = gameMap.getSpawnPoint();
@@ -107,8 +108,11 @@ public class GameScreen extends ScreenAdapter {
         handleInput();
 
         gameMap.update(delta);
+        dayCycleManager.update(delta);
+
         Vector2 characterPosition = getCharacterPosition();
         
+        gameMap.insideCheck(characterPosition, player.getWidth(), player.getHeight());
         gameMap.insideCheck(characterPosition, player.getWidth(), player.getHeight());
 
         checkInteract();
@@ -122,6 +126,7 @@ public class GameScreen extends ScreenAdapter {
         gameMap.render(camera);
         player.render(delta, camera);
         energyMeter.render();
+        dayCycleManager.render();
 
         batch.begin();
         if (currentTable != null){
@@ -204,6 +209,7 @@ public class GameScreen extends ScreenAdapter {
         batch.dispose();
         player.dispose();
         gameMap.dispose();
+        dayCycleManager.dispose();
         // Dispose other resources here
     }
 
